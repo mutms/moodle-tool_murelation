3702fe58fc4a02a60270daf4e8abed35a21e5927
Petr Skoda <petr@mutms.org>
2025-12-03 12:58:00 +0100

    Refactor autocomplete web services

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_murelation';
-$plugin->version = 2025112750;
+$plugin->version = 2025120350;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
@@ -36,5 +36,5 @@ $plugin->supported = [500, 501];
 $plugin->release = 'mu-5.0.3-02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025112750,
+    'tool_mulib' => 2025120350,
 ];
