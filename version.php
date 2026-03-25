2e74c624abeab9a325990bc133abf72ef46927fa
Petr Skoda <petr@mutms.org>
2026-03-25 16:36:24 +0100

    Prepare release

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_murelation';
-$plugin->version = 2026030145.01;
+$plugin->version = 2026032645;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
 $plugin->release = 'v4.5.10.02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026030145,
+    'tool_mulib' => 2026032645,
 ];
