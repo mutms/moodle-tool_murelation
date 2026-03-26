7c94f522fd8977fbf1a6f6286f62ad93fa3895ef
Petr Skoda <petr@mutms.org>
2026-03-26 16:37:30 +0100

    Add Moodle 5.2 compatibility

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_murelation';
 $plugin->version = 2026032650;
 $plugin->requires = 2024100700;
-$plugin->supported = [500, 501];
+$plugin->supported = [500, 502];
 
 $plugin->release = 'v5.0.6.03+';
 
